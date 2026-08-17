ba53b8b53ea01bfeec78cc584ed3e07bc15f2cb8
TolikSyedin <toliksyedin@gmail.com>
2026-08-17 15:28:30 +0300

    Remove unused ThemeToggle

--- components/theme/index.ts
+++ components/theme/index.ts
@@ -1,4 +1,3 @@
 export { ThemeProvider } from './theme-provider'
-export { ThemeToggle } from './theme-toggle'
 export { ChainToggle } from './chain-toggle'
 export { StrobeToggle } from './strobe-toggle'
