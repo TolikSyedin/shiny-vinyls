634fa7ba2640d71d8f332278fe3694bd385eee4c
TolikSyedin <toliksyedin@gmail.com>
2026-08-17 15:30:25 +0300

    Wire up the autocomplete prop on TextField and fix invalid fullname token

--- components/forms/review-form.tsx
+++ components/forms/review-form.tsx
@@ -63,7 +63,7 @@ export function ReviewForm() {
       <TextField
         id="name"
         label="Ім'я"
-        autocomplete="fullname"
+        autocomplete="name"
         placeholder={NAME_PLACEHOLDER}
         error={errors.name?.message}
         {...register('name')}
