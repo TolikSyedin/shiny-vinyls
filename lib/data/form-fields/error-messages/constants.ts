81dfbb877571d84f13b6a0ba5ec238965bba72c6
TolikSyedin <toliksyedin@gmail.com>
2026-08-19 16:22:36 +0300

    Shorten name related error message

--- lib/data/form-fields/error-messages/constants.ts
+++ lib/data/form-fields/error-messages/constants.ts
@@ -1,7 +1,7 @@
 export const ERROR_MESSAGES = {
   EMAIL_IS_INCORRECT: 'Некоректний email',
   PASSWORD_TOO_SHORT: 'Пароль занадто короткий',
-  NAME_TOO_SHORT: "Ім'я занадто коротке: очікується мінімум 2 символи",
+  NAME_TOO_SHORT: "Ім'я занадто коротке",
   PHONE_TOO_SHORT: 'Телефон занадто короткий',
   PHONE_CONTAINS_UNEXPECTED_VALUES: 'Телефон містить неприпустимі символи',
   RATING_NOT_DEFINED: 'Будь ласка, вкажіть вашу оцінку',
